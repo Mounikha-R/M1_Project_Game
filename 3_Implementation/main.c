ba41edae0f39f173398d7028353d92d6399d7774
Mounikha-R <59721857+Mounikha-R@users.noreply.github.com>
2022-02-12 18:34:32 +0530

    Create main.c

---
+++ 3_Implementation/main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "args.h"
+#include "data.h"
+#include "exit.h"
+#include "input.h"
+#include "output.h"
+#include "terminal.h"
+#include "timer.h"
+
+int main(int argc, char *argv[]) {
+    tt.time = 60;
+    tt.avg_length = 4.79;
+    tt.print_index = 0;
+    int filename = 1;
+    for (int i = 1; i < argc; i++) {
+        filename = parse_argument(argv[i]);
+    }
+    if (filename) {
+        print_help();
+        return 0;
+    }
+
+    init_terminal();
+    parse_text();
+    print_text();
+
+    while (1) {
+        process_keypress();
+        if (check_timer() || tt.pos == tt.length) {
+            print_score();
+        }
+    }
+
+    return 0;
+}
